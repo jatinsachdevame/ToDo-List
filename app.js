2aafc7b252832563b5dbd38c98493d9fd0bf66cf
Jatin Sachdeva <jatinsachdeva931@gmail.com>
2021-01-30 23:46:53 +0530

    Creating date module

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();	
 
-let items = ["Node JS", "Express JS"];
-let workItems = [];
+console.log(date)
+
+const items = ["Node JS", "Express JS"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true})) 
@@ -14,16 +17,7 @@ app.use(express.static("public"))
 
 app.get("/", function(req, res) {
 
-	let today = new Date();
-	
-	let options = {
-		weekday: "long",
-		day: "numeric",
-		month: "long"
-	}
-
-	let day = today.toLocaleDateString("en-US", options);
-
+	let day = date.getDate();
 
 	res.render("list", {listTitle: day, newListItems: items});
 })
